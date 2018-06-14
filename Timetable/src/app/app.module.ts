d1eed2ac3f0c32f2272c832948b87aa678b8b85c
Gerben den Boer <gerbendenboer@145-24-254-69.wlan.hro.nl>
2018-06-14 11:35:46 +0200

    -

--- Timetable/src/app/app.module.ts
+++ Timetable/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Firebase } from '@ionic-native/firebase'
+import { Firebase } from '@ionic-native/firebase';
 
 import { GooglePlus } from '@ionic-native/google-plus';
 
